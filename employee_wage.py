c05cb1bd66fd3fcccb6dec13c5916c770b9dd410
unknown <chandru151998@gmail.com>
2022-12-23 16:46:04 +0530

    [Chandru] daily wage

--- employee_wage.py
+++ employee_wage.py
@@ -1,29 +1,33 @@
-# Program to calculate Employee Wage
 import random
 
 
-def wage_for_month():
-    """
-    Function calculates wage of month
-    """
-    is_full_time = 1
-    is_part_time = 2
-    emp_rate_per_hour = 20
-    num_of_working_days = 20
-    total_emp_wage = 0
-    for days in range(num_of_working_days):
+def calc_wage():
+    """Function to calculate wage"""
+    total_emp_hrs = 0
+    max_working_hrs = 100
+    total_emp_days = 0
+    max_working_days = 20
+    total_wage = 0
+    emp_wage_per_hr = 15
+    while total_emp_hrs < max_working_hrs and total_emp_days < max_working_days:
+        total_emp_days = total_emp_days + 1
         emp_check = random.randrange(0, 3)
-        if emp_check == is_full_time:
-            emp_hrs = 8
-        elif emp_check == is_part_time:
-            emp_hrs = 4
-        else:
-            emp_hrs = 0
-
-        emp_wage = emp_hrs * emp_rate_per_hour
-        total_emp_wage += emp_wage
-
-    print(f'Daily wage is : {total_emp_wage}')
+        emp_hrs = check_attendance(emp_check)
+        total_emp_hrs += emp_hrs
+        total_wage += emp_hrs * emp_wage_per_hr
+    print(f'Total wage is : {total_wage}')
 
 
-wage_for_month()
+def check_attendance(emp_check):
+    """
+    Function to check employee working hours
+    """
+    switcher = {
+        0: 0,
+        1: 8,
+        2: 4
+    }
+    return switcher.get(emp_check)
+
+
+calc_wage()
