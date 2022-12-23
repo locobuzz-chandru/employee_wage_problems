7b666d1106f8e27d85051f99440a15463cbcb9dc
unknown <chandru151998@gmail.com>
2022-12-23 16:09:49 +0530

    [Chandru] daily wage

--- employee_wage.py
+++ employee_wage.py
@@ -1,17 +1,23 @@
 import random
 
 
-def attendance():
+def daily_wage():
     """
-    Function checks employee attendance
+    Function calculates daily wage
     :return: Employee Attendance
     """
     is_present = 1
+    emp_rate_per_hour = 20
+    emp_hrs = 0
     emp_check = random.randrange(0, 2)
     if emp_check == is_present:
         print("Employee is present")
+        emp_hrs = 8
     else:
         print("Employee is absent")
 
+    emp_wage = emp_hrs * emp_rate_per_hour
+    print("Employee Wage : ", emp_wage)
 
-attendance()
+
+daily_wage()
