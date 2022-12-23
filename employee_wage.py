a90dbada2583fc0f3e437d77e87cb2279c98d59b
unknown <chandru151998@gmail.com>
2022-12-23 16:13:33 +0530

    [Chandru] daily wage

--- employee_wage.py
+++ employee_wage.py
@@ -6,13 +6,17 @@ def daily_wage():
     Function calculates daily wage
     :return: Employee Attendance
     """
-    is_present = 1
+    is_full_time = 1
+    is_part_time = 2
     emp_rate_per_hour = 20
     emp_hrs = 0
-    emp_check = random.randrange(0, 2)
-    if emp_check == is_present:
-        print("Employee is present")
+    emp_check = random.randrange(0, 3)
+    if emp_check == is_full_time:
+        print("Full time")
         emp_hrs = 8
+    elif emp_check == is_part_time:
+        print("Part time")
+        emp_hrs = 4
     else:
         print("Employee is absent")
 
